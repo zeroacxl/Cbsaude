0f6aa3898516405f5ffbda5e82a33b41b8eb758b
zeroacxl <103471453+zeroacxl@users.noreply.github.com>
2025-10-22 04:15:22 -0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBexxAzdyVtthakeOVfTf-PxnlQ5rNynRQ",
-  authDomain: "cbsaudecooperados.firebaseapp.com",
-  projectId: "cbsaudecooperados",
-  storageBucket: "cbsaudecooperados.firebasestorage.app",
-  messagingSenderId: "558244165893",
-  appId: "1:558244165893:web:2e10e9560c896f43e6df6c"
-};
